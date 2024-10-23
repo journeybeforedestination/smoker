c333b256476a5d5a509f532566ddb87e0b9f4a53
Jared Campbell <jaredc@hey.com>
2024-10-22 19:11:06 -0500

    Use hasmap for temp launch state

--- app/controllers/launches_controller.rb
+++ app/controllers/launches_controller.rb
@@ -7,11 +7,10 @@ class LaunchesController < ApplicationController
   end
 
   def initiate
-    iss = params[:iss]
-    launch = params[:launch]
+    launch_data = launch_parms
 
     # this will break on windows... do we really need a gem for this???
-    conn = Faraday.new(url: File.join(iss, ".well-known/smart-configuration")) do |builder|
+    conn = Faraday.new(url: File.join(launch_data[:iss], ".well-known/smart-configuration")) do |builder|
       builder.request :json
       builder.response :json
       builder.response :raise_error
@@ -20,18 +19,13 @@ class LaunchesController < ApplicationController
 
     # todo handle errors from remote server
     smart_config = conn.get
-    authorization_endpoint = smart_config.body["authorization_endpoint"]
-    token_endpoint = smart_config.body["token_endpoint"]
+    launch_data[:authorization_endpoint] = smart_config.body["authorization_endpoint"]
+    launch_data[:token_endpoint] = smart_config.body["token_endpoint"]
 
-    state = SecureRandom.uuid.delete("-")
-    pkce = Digest::SHA256.hexdigest "ourcode"
+    launch_data[:state] = SecureRandom.uuid.delete("-")
+    launch_data[:pkce] = Digest::SHA256.hexdigest "ourcode"
 
-    @launch = Launch.new(iss: iss,
-                         launch: launch,
-                         authorization_endpoint: authorization_endpoint,
-                         token_endpoint: token_endpoint,
-                         state: state,
-                         pkce: pkce)
+    @launch = Launch.new(launch_data)
 
     # ex target: https://launch.smarthealthit.org/v/r4/auth/authorize?response_type=code&client_id=whatever&scope=patient%2F*.*%20user%2F*.*%20launch%20launch%2Fpatient%20launch%2Fencounter%20openid%20fhirUser%20profile%20offline_access&redirect_uri=https%3A%2F%2Flaunch.smarthealthit.org%2Fsample-app&aud=https%3A%2F%2Flaunch.smarthealthit.org%2Fv%2Fr4%2Ffhir&state=kuPNPLBtO2TqvSOF&launch=WzAsIiIsIiIsIkFVVE8iLDAsMCwwLCIiLCIiLCIiLCIiLCIiLCIiLCIiLDAsMSwiIl0&code_challenge=SWTzkX6Kdd58nGK6zIq0iHH7YAr8QGbwmFBUhRk6EIk&code_challenge_method=S256
     # todo handle failure saving
@@ -40,5 +34,11 @@ class LaunchesController < ApplicationController
       target = @launch.CalcAuthRedirect(app_url)
       redirect_to target, allow_other_host: true
     end
+  end
+
+  private
+
+  def launch_parms
+    params.permit(:iss, :launch)
   end
 end
