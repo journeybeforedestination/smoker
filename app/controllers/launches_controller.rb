db6ee61126a6979dabead71d1330f8a5ef439eb6
Jared Campbell <jaredc@hey.com>
2024-10-26 13:27:52 -0500

    redirect to launch show
    get ready to parse code and trade for token.

--- app/controllers/launches_controller.rb
+++ app/controllers/launches_controller.rb
@@ -4,6 +4,7 @@ class LaunchesController < ApplicationController
   end
 
   def show
+    @launch = Launch.find(params[:id])
   end
 
   def initiate
@@ -27,10 +28,9 @@ class LaunchesController < ApplicationController
 
     @launch = Launch.new(launch_data)
 
-    # ex target: https://launch.smarthealthit.org/v/r4/auth/authorize?response_type=code&client_id=whatever&scope=patient%2F*.*%20user%2F*.*%20launch%20launch%2Fpatient%20launch%2Fencounter%20openid%20fhirUser%20profile%20offline_access&redirect_uri=https%3A%2F%2Flaunch.smarthealthit.org%2Fsample-app&aud=https%3A%2F%2Flaunch.smarthealthit.org%2Fv%2Fr4%2Ffhir&state=kuPNPLBtO2TqvSOF&launch=WzAsIiIsIiIsIkFVVE8iLDAsMCwwLCIiLCIiLCIiLCIiLCIiLCIiLCIiLDAsMSwiIl0&code_challenge=SWTzkX6Kdd58nGK6zIq0iHH7YAr8QGbwmFBUhRk6EIk&code_challenge_method=S256
     # todo handle failure saving
     if @launch.save
-      app_url = URI.join("http://" + request.host_with_port, launches_path)
+      app_url = URI.join("http://" + request.host_with_port, launch_path(@launch))
       target = @launch.CalcAuthRedirect(app_url)
       redirect_to target, allow_other_host: true
     end
